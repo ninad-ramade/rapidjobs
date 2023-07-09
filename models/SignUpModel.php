b6d64877a1c2e83f762878f8defa6258afb46b69
Ninad Ramade <ninad.ramade@pegasusone.com>
2023-07-10 00:33:52 +0530

    laravel to core php change

--- models/SignUpModel.php
+++ models/SignUpModel.php
@@ -42,6 +42,7 @@ class SignUpModel extends Database {
     public function login($username, $password) {
         $query = "SELECT * FROM candidates WHERE email = '" . $username . "'";
         $candidate = $this->fetchOne($query);
+        var_dump($candidate);exit;
         if(empty($candidate)) {
             $this->response['status'] = 0;
             $this->response['message'] = SignupErrorMessages::_LOGIN_USER_NOT_FOUND_;
