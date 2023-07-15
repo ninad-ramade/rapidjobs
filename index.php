a2a50670d5556b3597a4c42dcb5b4acfd0eb6be5
Ninad Ramade <ninad.ramade@pegasusone.com>
2023-07-15 19:12:02 +0530

    Home page changes

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- FAVICONS ICON -->
     <link rel="icon" href="assets/images/favicon.ico" type="image/x-icon" />
-    <link rel="shortcut icon" type="image/x-icon" href="assets/images/favicon.png')" />
+    <link rel="shortcut icon" type="image/x-icon" href="assets/images/favicon.png" />
 
     <title>RapidJobs</title>
 
